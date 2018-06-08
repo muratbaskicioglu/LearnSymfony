661c0de174d8e824a65f8c64ef3e7f5f9df9c2e6
Murat Baskıcıoğlu <muratbaskicioglu@gmail.com>
2018-06-08 23:09:22 +0300

    Add commits to article page

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -27,8 +27,15 @@ class ArticleController extends AbstractController
      */
     public function show($slug)
     {
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
         ]);
     }
 }
