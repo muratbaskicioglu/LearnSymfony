04fec4ae05667fb52164dbd26c3ac8ae968582a3
Murat Baskıcıoğlu <muratbaskicioglu@gmail.com>
2018-06-08 22:11:46 +0300

    Add a twig template page

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,9 +9,10 @@
 namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -26,9 +27,8 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'Future page to show article: %s',
-            $slug
-        ));
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+        ]);
     }
 }
